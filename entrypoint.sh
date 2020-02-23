0529e4c0ed5e26ce5e5fa9ccb20a3aef5b2adb74
Julian Ahrens <julianahrens@users.noreply.github.com>
2020-02-23 15:52:25 +0100

    fix: make entrypoint executable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-pdflatex --halt-on-error $1
+pdflatex --output-directory=$2 --halt-on-error $1
 biber $1
-pdflatex --halt-on-error $1
-pdflatex --halt-on-error $1
+pdflatex --output-directory=$2 --halt-on-error $1
+pdflatex --output-directory=$2 --halt-on-error $1
